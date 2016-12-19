0aeb12f381f2068ca5fe5bfc090c53c5f38d46db
EnPing Hsieh <shieh.npin@gmail.com>
2016-12-20 01:20:38 +0800

    Initial commit

---
+++ app/src/main/java/com/enping/simplechat/ChatApplication.java
@@ -0,0 +1,27 @@
+package com.enping.simplechat;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+import com.parse.interceptors.ParseLogInterceptor;
+
+/**
+ * Created by Chjeng-Lun SHIEH on 2016/12/19.
+ */
+
+public class ChatApplication extends Application {
+
+        @Override
+        public void onCreate() {
+            super.onCreate();
+
+            ParseObject.registerSubclass(Message.class);
+            // set applicationId and server based on the values in the Heroku settings.
+            // any network interceptors must be added with the Configuration Builder given this syntax
+            Parse.initialize(new Parse.Configuration.Builder(this)
+                    .applicationId("93bd0b59a90d46b1999e484431b83f41") // should correspond to APP_ID env variable
+                    .addNetworkInterceptor(new ParseLogInterceptor())
+                    .server("https://simplechatclient.herokuapp.com/parse/").build());
+        }
+}
